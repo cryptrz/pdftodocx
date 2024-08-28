871a7e3aa8f8ea9449d3fab17f049b089d11aeba
Franck Ridel <68272257+cryptrz@users.noreply.github.com>
2024-08-28 09:01:27 +0200

    First commit

---
+++ pdftodoc.py
@@ -0,0 +1,19 @@
+import sys
+from pdf2docx import Converter
+
+filename = input('What is the fulle path of the PDF file to convert? ')
+pdf_file = filename
+docx_file = filename + '_converted.docx'
+
+# Check filename
+if filename == "":
+    print("The path can't be empty")
+    sys.exit()
+
+# convert pdf to docx
+try:
+    cv = Converter(pdf_file)
+    cv.convert(docx_file) 
+    cv.close()
+except:
+    print("File not found")
